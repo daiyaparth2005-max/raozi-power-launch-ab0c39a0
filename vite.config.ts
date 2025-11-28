8abff8622fb0a34717f4d71938a5661d203e0d14
daiyaparth2005-max <daiyaparth2005@gmail.com>
2025-11-28 14:27:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig(({ mode }) => ({
   server: {
   host: '::',
   port: 8080,
-  allowedHosts: ['raozi.in', 'www.raozi.in']
 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
